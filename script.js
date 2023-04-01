a9e5afd28273a88bec2297ea3adf8872f77f6f8a
gillvasoff <gillvasoff@gmail.com>
2023-04-01 12:24:35 -0400

    start claculator operation logic

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function divide(a,b) {
     else { return a/b };
 }
 
+function power(a,b){
+    return Math.pow(a,b);
+}
+
 function operate(a, op, b) {
 
     switch(op){
@@ -30,7 +34,22 @@ function operate(a, op, b) {
     }
 }
 
+function onClickNum(){
+    currentNum += this.text;
+}
+
+
 let a = 0;
 let op = "";
 let b = 0;
+let currentNum = "" ;
+
+let numbers = document.querySelector(".btnNum");
+operators.forEach(element => { Element.addEventListener('click', onClickNum)
+})
+
+let operators = document.querySelector(".btnOp");
+operators.forEach(element => { Element.addEventListener('click', onClickOperator)
+    
+});
 
